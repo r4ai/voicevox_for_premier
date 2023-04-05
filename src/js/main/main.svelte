077c3a7a2ea066d801c980950e272ca59667d3c9
rai <mj82xx@gmail.com>
2023-04-05 17:29:42 +0900

    formをちょっと作成した

--- src/js/main/main.svelte
+++ src/js/main/main.svelte
@@ -1,1 +1,26 @@
-<h1 class="text-red-400">Welcome to Bolt CEP!</h1>
+<script lang="ts">
+  let selectedValue = "";
+</script>
+
+<main data-theme="business">
+  <form class="form-control w-full max-w-xs flex gap-4">
+    <div>
+      <label class="label" for="character-select">
+        <span class="label-text">Select Character</span>
+      </label>
+      <select class="select select-bordered select-sm" id="character-select">
+        <option>Star Wars</option>
+        <option>Harry Potter</option>
+        <option>Lord of the Rings</option>
+        <option>Planet of the Apes</option>
+        <option>Star Trek</option>
+      </select>
+    </div>
+    <div class="w-full text-center">
+      <input type="submit" class="btn btn-sm btn-primary" value="Generate" />
+    </div>
+  </form>
+</main>
+
+<style>
+</style>
