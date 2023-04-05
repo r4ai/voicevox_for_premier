6969a5420b7929d8e1b2c8ad5a80d57ef0a6a649
rai <mj82xx@gmail.com>
2023-04-05 20:24:00 +0900

    api

---
+++ src/js/openapi/models/index.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './';
+export * from './AccentPhrase';
+export * from './AudioQuery';
+export * from './DownloadableLibrary';
+export * from './EngineManifest';
+export * from './HTTPValidationError';
+export * from './LicenseInfo';
+export * from './Mora';
+export * from './MorphableTargetInfo';
+export * from './ParseKanaBadRequest';
+export * from './Preset';
+export * from './Speaker';
+export * from './SpeakerInfo';
+export * from './SpeakerStyle';
+export * from './SpeakerSupportPermittedSynthesisMorphing';
+export * from './SpeakerSupportedFeatures';
+export * from './StyleInfo';
+export * from './SupportedDevicesInfo';
+export * from './SupportedFeatures';
+export * from './UpdateInfo';
+export * from './UserDictWord';
+export * from './ValidationError';
+export * from './WordTypes';
